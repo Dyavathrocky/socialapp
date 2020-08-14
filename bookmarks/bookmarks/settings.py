0cd9ccc747151b5490eeceb0c7256057d06abf28
dyavathrocky <davathrak@gmail.com>
2020-08-14 10:22:46 +0530

    added authentication urls

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -125,3 +125,11 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+"""EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'davathrak200@gmail.com'
+EMAIL_HOST_PASSWORD = 'rakesh@123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True"""
+
