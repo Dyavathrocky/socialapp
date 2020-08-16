9312785afbb8099b252acc9ef8078fe21e4df3dc
dyavathrocky <davathrak@gmail.com>
2020-08-16 10:04:20 +0530

    added image model to project

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                                 related_name='images_created',
                                 on_delete=models.CASCADE)
+    
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200,
                                         blank=True)
@@ -13,6 +18,11 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True,
                                 db_index=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
